c0638f41e2df0f4f5854209b9dc00d5c0394f701
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-05-16 06:19:07 +0530

    Update chat_engine.js

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,7 +4,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://3.22.183.158/:5000');
 
         if (this.userEmail){
             this.connectionHandler();
